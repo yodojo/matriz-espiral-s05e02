886c05910ece8b65131410d27bb27ab4b1c6d30b
Fabian Carlos <fabian.pow@gmail.com>
2015-02-07 18:30:14 -0300

    README, gera matriz, some bugs

---
+++ lib/matriz.rb
@@ -0,0 +1,46 @@
+class Matriz
+	attr_accessor :columns, :lines, :vetor
+
+	def initialize(columns, lines)
+		@columns = columns
+		@lines = lines
+		@vetor = []
+	end
+
+	def retorna_matriz
+		i = 0
+		(1..@lines).each do |linha|
+			@vetor[linha] = []
+			(1..@columns).each do |coluna| 
+				
+				if linha == 1
+					i += 1
+					@vetor[linha][coluna] = i
+				elsif coluna == @columns
+					i += 1
+					@vetor[linha][coluna] = i
+
+				elsif linha == @lines
+					@vetor[linha][coluna] =  i
+
+				else
+					@vetor[linha][coluna] = i
+				end
+
+
+				if linha != 1
+					@vetor[linha][1] = (@lines*@columns) - linha
+				end
+			end
+
+			puts @vetor[linha].join(' ')
+		end
+
+		
+		@vetor
+
+	
+		# puts @vetor[linha].join(' ')
+
+	end
+end
---
+++ spec/lib/matriz_espiral_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require 'matriz'
+
+describe "Matriz" do
+		
+	it "Matriz de 3 e 4" do
+		@matriz = Matriz.new(3, 4)
+
+		expect(@matriz.columns).to be 3
+		expect(@matriz.lines).to be 4
+	end
+
+	it "Matriz de 5 e 6" do
+		@matriz = Matriz.new(5, 6)
+
+		expect(@matriz.columns).to be 5
+		expect(@matriz.lines).to be 6
+	end
+
+	it "Retornar uma matriz espiral de 3x4" do	
+		@matriz = Matriz.new(3, 4)
+		@matriz.retorna_matriz
+	end
+
+	it "Retornar uma matriz espiral de 5x6" do	
+		@matriz = Matriz.new(5, 6)
+		@matriz.retorna_matriz
+	end
+
+
+end
